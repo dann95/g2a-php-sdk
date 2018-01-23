19dd3b2bcf8f3085a32ea491290c398548113182
Daniel Rubin <danielrubin95@hotmail.com>
2018-01-23 17:29:58 -0200

    add a provider for Laravel

---
+++ SDK/Integrations/Laravel/G2aServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+
+namespace G2A\Integrations\Laravel;
+
+use G2A\Sdk;
+use Illuminate\Support\ServiceProvider;
+
+class G2aServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        $this->publishes([__DIR__.'/laravel-config.php' => 'g2a.php']);
+    }
+
+    public function register()
+    {
+        $this->app->bind('G2A', function () {
+            $configs = config('g2a');
+            return new Sdk(
+                $configs['hash'],
+                $configs['email'],
+                $configs['secret'],
+                $configs['environment']
+            );
+        });
+    }
+}
---
+++ SDK/Integrations/Laravel/laravel-config.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * to obtain hash and secret, go to:
+ * https://www.test.pay.g2a.com/setting/merchant (in case of SANDBOX)
+ * https://www.pay.g2a.com/setting/merchant (in case of PRODUCTION)
+ * email is your account e-mail.
+ */
+
+return [
+    'hash'          => '',
+    'secret'        => '',
+    'email'         => '',
+    'environment'   => 'SANDBOX', // SANDBOX || PRODUCTION
+];
